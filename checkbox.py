e7d84982156d497cb63cef211db6ef6efa7cc199
abhinavvkp <akp889486@gmail.com>
2026-09-19 12:12:57 +0530

    Create checkbox.py

---
+++ checkbox.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+agree = st.checkbox("I agree to the terms and conditions")
+
+if agree:
+    st.write("Thank you! You agreed.")
+else:
+    st.write("Please agree to continue.")
